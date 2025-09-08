d4a6ec83908c9fd99986b7f630b42ab8cd389a44
BaduleanuGeorge <117219312+BaduleanuGeorge@users.noreply.github.com>
2025-09-08 13:12:32 +0100

    Delete QA-Portfolio/e2e directory

--- QA-Portfolio/e2e/login-success.cy.js
+++
@@ -1,9 +0,0 @@
-describe("template spec", () => {
-  //Successfull Login
-  it("should log in successfully", () => {
-    //Fill in the right credentials and click button
-    cy.login();
-    cy.url().should("include", "/dashboard/index");
-    cy.get(".oxd-topbar-header-title").should("contain.text", "Dashboard");
-  });
-});
